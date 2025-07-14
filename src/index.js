7deab4562250d622708f925cd9f5ac28477c6ec8
Cristian Antir <antircristian@gmail.com>
2025-07-14 22:32:37 +0200

    info

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
   console.info("DOM content loaded.");
+  console.log("info", window.navigator.userAgent);
 });
